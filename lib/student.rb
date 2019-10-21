e2fe3de4593c2d9883be6e4fcc5bfe87ae3797e8
clean-command-1851 <ed7901979t@gmail.com>
2019-10-21 17:06:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,46 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
+ attr_accessor :name, :grade
+attr_reader :id
+ 
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @album = grade
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS stuedent (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+ 
+  def save
+    sql = <<-SQL
+      INSERT INTO student (name, grade)
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM student")[0][0]
+  end
+ 
+  def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+ 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
 
 end
